e7e35899ff2160e49352eba536b7fb2000a8d296
Unknown <max-kersten@live.nl>
2018-02-21 21:26:08 +0100

    [+] Connection.cs [/] Country.cs

---
+++ Assets/Scripts/Country/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maskirovka
+{
+    public class Connection : MonoBehaviour {
+        public Neighbour neighbour;
+        public Country country;
+
+        void Update() {
+
+        }
+    }
+}
