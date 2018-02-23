54cbf65262d9e1e6e6386cc829f6504e71f79d9d
Max Kersten <max-kersten@live.nl>
2018-02-24 00:14:36 +0100

    [/] Connection.cs v1.0

--- Assets/Scripts/Country/Connection.cs
+++ Assets/Scripts/Country/Connection.cs
@@ -5,11 +5,61 @@ using UnityEngine;
 namespace Maskirovka
 {
     public class Connection : MonoBehaviour {
-        public Neighbour neighbour;
+
+        [Header("Country connections")]
+        public Country neighbour;
         public Country country;
+        public LineRenderer connection;
+        public Catagorie catagorie;
 
-        void Update() {
+        [Header("Reputation settings")]
+        public float minRep;
+        public float maxRep;
 
+        void Awake()
+        {
+            connection = GetComponent<LineRenderer>();
+            connection.positionCount = 2;
+            connection.SetPosition(0, country.transform.position);
+            connection.SetPosition(1, neighbour.transform.position);
+        }
+
+        void Update()
+        {
+            float temp1 = Search(country, neighbour);
+            float temp2 = Search(neighbour, country);
+
+            if(temp1 < minRep || temp1 > maxRep)
+            {
+                if (temp2 < minRep || temp2 > maxRep)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+
+        float Search(Country country, Country neighbour)
+        {
+            for (int i = 0; i < neighbour.neighbours.Length; i++)
+            {
+                if (country.neighbours[i].neighbour == neighbour)
+                {
+                    if(catagorie == Catagorie.A)
+                    {
+                        return neighbour.neighbours[i].reputation.x;
+                    }
+                    else if (catagorie == Catagorie.B)
+                    {
+                        return neighbour.neighbours[i].reputation.y;
+                    }
+                    else if (catagorie == Catagorie.C)
+                    {
+                        return neighbour.neighbours[i].reputation.z;
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
