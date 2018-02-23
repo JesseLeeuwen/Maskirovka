7f8d60ea12901ff0496a6a7d75b7c5ab4b1c006f
Unknown <hairpasta@hotmail.com>
2018-02-24 00:42:37 +0100

    Full button animation, updated popup prefab

---
+++ Assets/Scripts/UI/ButtonText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonText : MonoBehaviour, 
+IPointerDownHandler, IPointerClickHandler, IPointerExitHandler {
+
+	public Text text;
+	public int buttonHeight = 11;
+	private Vector3 offset;
+	private bool isDown;
+
+	void Awake(){
+		offset = new Vector3(0,-buttonHeight,0);
+	}
+
+	public void OnPointerDown(PointerEventData eventData){
+		text.transform.position= text.transform.position + offset;
+		isDown = true;
+	}
+
+	public void OnPointerClick(PointerEventData eventData){
+		if (isDown){Release();};
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		if (isDown){Release();};
+	}
+
+
+	public void Release(){
+		isDown = false;
+		text.transform.position= text.transform.position - offset;
+	}
+ 
+
+
+}
