a100a7a24fb1ae98070cc2bcfa6636c1d05891d0
Jesse van Leeuwen <sf.jessenl@gmail.com>
2018-02-27 14:53:02 +0100

    setup country info UI

---
+++ Assets/Scripts/UI/NeighbourInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Maskirovka.UI
+{
+	public class NeighbourInfo : MonoBehaviour 
+	{
+		[SerializeField]
+		private Image icon;
+		[SerializeField]
+		private RectTransform[] bars;
+		
+		private Neighbour neighbour;
+
+		public void Init(Neighbour neighbour)
+		{
+			this.neighbour = neighbour;
+		}
+		
+		void Update () 
+		{
+			
+		}
+	}
+}
---
+++ Assets/Scripts/UI/ReputationTable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Maskirovka.UI
+{
+
+	public class ReputationTable : MonoBehaviour 
+	{
+		[SerializeField]
+		private Image country;
+		[SerializeField]
+		private float[] reputation;
+		[SerializeField]
+		private RectTransform[] bars;
+
+		public void Init( Country country )
+		{
+			for(int i = 0; i < 3; ++i)
+				reputation[i] = Random.value * 100;
+		}
+		
+		void Update () 
+		{
+			for(int i = 0; i < 3; ++i)
+			{
+				float height = bars[i].sizeDelta.y;
+				Vector2 target = new Vector2( reputation[i] * 1.80f, height );
+				bars[i].sizeDelta = Vector2.MoveTowards( bars[i].sizeDelta, target, Time.deltaTime * 50.0f );
+			}
+		}
+	}
+}
