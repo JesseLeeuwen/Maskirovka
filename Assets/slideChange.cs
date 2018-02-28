2dee736ab0a0eaff6df5ae4432e86a9358e8d2c1
Unknown <hairpasta@hotmail.com>
2018-02-28 21:20:11 +0100

    visual slider, fixed news generation

---
+++ Assets/slideChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slideChange : MonoBehaviour {
+
+
+	private LineRenderer lr;
+	public GameObject handle;
+	public GameObject marker;
+
+	// Use this for initialization
+	void Start () {
+		lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mPos = marker.transform.position;
+		mPos.y=handle.transform.position.y;
+		lr.SetPosition(0,mPos);
+		lr.SetPosition(1,handle.transform.position);
+	}
+}
