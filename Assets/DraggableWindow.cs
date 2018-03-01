8c43e292613518d89e926aca35af2d6d63a47a02
Unknown <jasper@oprel.work>
2018-03-01 13:12:26 +0100

    exit, restart game

---
+++ Assets/DraggableWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+ using UnityEngine.UI;
+ using UnityEngine.EventSystems;
+ 
+ [RequireComponent(typeof(RectTransform))]
+ public class DraggableWindow : MonoBehaviour,
+ IDragHandler
+ 
+ {
+     private RectTransform rect;
+ 
+     public void Awake()
+     {
+         rect = GetComponent<RectTransform>();
+     }
+ 
+     public void OnDrag(PointerEventData eventData)
+     {
+ 
+         Vector3 currentPosition = rect.position;
+         currentPosition.x += eventData.delta.x/50;
+         currentPosition.y += eventData.delta.y/50;
+         rect.position = currentPosition;
+		 Debug.Log("Moved UI windows");
+     }
+ }
