d863374b5fd5eddbe5ce2777078aca24f3963572
Unknown <jasper@oprel.work>
2018-03-20 17:33:46 +0100

    neihgbour markers move into sight, map prefab applied

--- Assets/Scripts/slideChange.cs
+++ Assets/Scripts/slideChange.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class slideChange : MonoBehaviour {
 
-
-	private LineRenderer lr;
-	public GameObject handle;
 	public GameObject marker;
+	public GameObject handle;
+	private RectTransform trans;
 
-	// Use this for initialization
-	void Start () {
-		lr = GetComponent<LineRenderer>();
+	void Start(){
+		trans = GetComponent<RectTransform>();
 	}
-	
 	// Update is called once per frame
 	void Update () {
+		float xMarker = marker.GetComponent<RectTransform>().position.x;
+		float xHandle = handle.GetComponent<RectTransform>().position.x;
+
+		Vector2 pos = trans.position;
+		pos.x = Mathf.Min(xMarker,xHandle);
+		trans.position=pos;	
+	
+		Vector2 size= trans.sizeDelta;
+		size.x = Mathf.Max(xMarker,xHandle)-pos.x;
+		trans.sizeDelta = size;
+
+		/* 
+		float x = Mathf.Max(marker.transform.position.x,)
 		Vector3 mPos = marker.transform.position;
 		mPos.y=handle.transform.position.y;
 		lr.SetPosition(0,mPos);
 		lr.SetPosition(1,handle.transform.position);
+		*/
 	}
 }
