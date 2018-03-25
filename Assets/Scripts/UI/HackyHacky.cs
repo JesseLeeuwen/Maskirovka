d8bd474e0f57dd331a15374ff5b57dc52e6c5b28
Max Kersten <max-kersten@live.nl>
2018-03-25 23:20:08 +0200

    [+] Spawn set catagorie items on fixed amount

---
+++ Assets/Scripts/UI/HackyHacky.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Maskirovka.News;
+using Maskirovka;
+using Maskirovka.Utility;
+
+using Maskirovka.Selector;
+
+namespace Maskirovka.UI
+{
+    public class HackyHacky : MonoBehaviour {
+        public Catagorie whatYouWant;
+        private NewsFeedItem news;
+
+        // Update is called once per frame
+        void Start() {
+            news = GetComponent<NewsFeedItem>();
+            news.catagorie = whatYouWant;
+            news.contentText.text = getCopy(whatYouWant, Mathf.RoundToInt(news.value), news.subject, Random.Range(7, 10));
+            news.ValueBar.color = CatagorieSettings.GetColor(whatYouWant);
+            news.icon_L.sprite = CatagorieSettings.GetIconLeft(whatYouWant);
+            news.icon_R.sprite = CatagorieSettings.GetIconRight(whatYouWant);
+        }
+
+        string getCopy(Catagorie cat, int value, Country country, int length)
+        {
+            string[] left = CatagorieSettings.GetKeywordsLeft(cat);
+            string[] right = CatagorieSettings.GetKeywordsRight(cat);
+            string line = getWord(left, right, value);
+            line = char.ToUpper(line[0]) + line.Substring(1); //capitalize first letter
+            for (int i = 1; i < length; i++)
+            {
+                line += " ";
+                line += getWord(left, right, value);
+            };
+            if (length > 5)
+                line += ".";
+            return line;
+        }
+
+        string getWord(string[] left, string[] right, int value)
+        {
+            if (Random.Range(0, 100) < value)
+            {
+                return left[Random.Range(0, left.Length)];
+            }
+            return right[Random.Range(0, right.Length)];
+        }
+
+    }
+}
--- Assets/Scripts/UI/TwitterFeed.cs
+++ Assets/Scripts/UI/TwitterFeed.cs
@@ -7,22 +7,45 @@ namespace Maskirovka.UI
 	public class TwitterFeed : MonoBehaviour 
 	{
 		[SerializeField]
-		private GameObject prefab;
+		private GameObject normalPrefab;
+        [SerializeField]
+        private GameObject tuturialPrefab;
 		[SerializeField]
 		private RectTransform content;
 
+        private int Amount;
+        [Header("Max to spawn of set categorie")]
+        public int maxToSpawn;
+
 		public NewsFeedItem NewTweet(NewsData newMessage, Country country)
 		{
-			GameObject n = Instantiate(prefab);
-			n.transform.SetParent( transform, false);
-			n.transform.SetAsFirstSibling( );
+            if(Amount < maxToSpawn)
+            {
+                GameObject n = Instantiate(tuturialPrefab);
+                n.transform.SetParent(transform, false);
+                n.transform.SetAsFirstSibling();
 
-            NewsFeedItem item = n.GetComponent<NewsFeedItem>();
-            item.Init(country, newMessage);
+                NewsFeedItem item = n.GetComponent<NewsFeedItem>();
+                item.Init(country, newMessage);
 
-			content.anchoredPosition = Vector2.zero;
-            content.sizeDelta = new Vector2( content.sizeDelta.x, content.childCount * 140 );
-            return item;
+                content.anchoredPosition = Vector2.zero;
+                content.sizeDelta = new Vector2(content.sizeDelta.x, content.childCount * 140);
+                Amount++;
+                return item;
+            }
+            else
+            {
+                GameObject n = Instantiate(normalPrefab);
+                n.transform.SetParent(transform, false);
+                n.transform.SetAsFirstSibling();
+
+                NewsFeedItem item = n.GetComponent<NewsFeedItem>();
+                item.Init(country, newMessage);
+
+                content.anchoredPosition = Vector2.zero;
+                content.sizeDelta = new Vector2(content.sizeDelta.x, content.childCount * 140);
+                return item;
+            }
 		}
 	}
 }
