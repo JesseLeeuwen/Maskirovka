b32b6fd84d9af0597c17811071a13cb5b3bbe787
Unknown <max-kersten@live.nl>
2018-03-27 15:03:45 +0200

    [+] Only Baker set as selectable in tutorial

---
+++ Assets/Scripts/UI/SetCorrectLayerTutorial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using Maskirovka.News;
+using Maskirovka;
+using Maskirovka.Utility;
+using Maskirovka.Selector;
+
+namespace Maskirovka.UI
+{
+    public class SetCorrectLayerTutorial : MonoBehaviour
+    {
+        void Update()
+        {
+            if (gameObject.GetComponent<NewsFeedItem>().subject.tag == "Baker")
+            {
+                gameObject.layer = 8;
+            } 
+    
+    }
+    }
+}
+
